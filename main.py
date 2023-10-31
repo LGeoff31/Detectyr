954d32c3670053e69527214bed77e2363b41fc1e
LGeoff31 <geoffrey31415@gmail.com>
2023-10-31 01:31:39 -0400

    ✅ Finalized code for calculating error expression

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# # Math IA: Estimating # of Gumballs | Nov 20+ 2020
+# ECE 198 | Calculating Parabola of best fit | Oct 25 | Geoffrey Lee
 
 def coefficients_in_expansion(x, y):
     return {
@@ -16,8 +16,8 @@ def coefficients_in_expansion(x, y):
 
 
 # Assign points into array
-points = [[11.83, 5.8], [12.92, 4.75], [14.07, 3.99],
-          [15.46, 3.55], [16.9, 3.7], [18.17, 4.3], [19.3, 5.1]]
+points = [[26, 55], [30, 60], [35, 65],
+          [38, 70], [42, 75], [45, 80]]
 # Intialize coeffients 0
 result = {
     'a_squared': 0,
@@ -30,7 +30,7 @@ result = {
     'c_squared': 0,
     'c': 0,
     'constant': 0}
-# Loop through points+ sum like-coefficients
+# Part 2: Loop through points + sum like-coefficients
 for x, y in points:
     current_coefficients = coefficients_in_expansion(x, y)
     result['a_squared'] += current_coefficients['a_squared']
@@ -43,5 +43,5 @@ for x, y in points:
     result['c_squared'] += current_coefficients['c_squared']
     result['c'] += current_coefficients['c']
     result['constant'] += current_coefficients['constant']
-# Print E(a+b+c)
+# Print E(a+b+c) aka error expression
 print(f'Simplified equation E(a+b+c): \n{round(result["a_squared"]+6)}a^2 + {round(result["ab"]+6)}ab + {round(result["ac"]+6)}ac + {round(result["a"]+6)}a + {round(result["b_squared"]+6)}b^2 + {round(result["bc"]+6)}bc + {round(result["b"]+6)}b + {round(result["c_squared"]+6)}c^2 + {round(result["c"]+6)}c + {round(result["constant"]+6)}')
